71f2c996cef243b06e09b31fad7da73452c7cc99
Danny Leshem <dleshem@gmail.com>
2020-12-30 01:29:12 +0200

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,45 +1,52 @@
-import fs from 'fs'
-import _ from 'lodash'
-import BookDownloader from './BookDownloader'
+import fs from 'fs';
+import _ from 'lodash';
+import BookDownloader from './BookDownloader';
 
 
-const readFile = (path, encoding) => {
+const readFile = async (path, encoding) => {
 	return new Promise((resolve, reject) => {
 		fs.readFile(path, encoding, (err, data) => {
 			if (err) {
-				reject(err)
+				reject(err);
 			} else {
-				resolve(data)
+				resolve(data);
 			}
-		})
-	})
-}
+		});
+	});
+};
 
-readFile('settings.json', 'utf8').then((str) => {
-	const {asin, queries, booksPath, auths} = JSON.parse(str)
+(async () => {
+	try {
+		for (let i = 0; i < 100; ++i) {
+			const str = await readFile('settings.json', 'utf8');
 
-	const auth = _.sample(auths)
-
-	console.log(`== booksPath: ${booksPath} ==`);
-	console.log(`== asin: ${asin} ==`);
-	console.log(`== auth: ${JSON.stringify(auth)} ==`);
-
-	const downloader = new BookDownloader({booksPath, asin, timeout: 10000})
-	downloader.init().then((value) => {
-		downloader.printInfo()
-		return downloader.useQuery(queries[0])
-	}).then(() => {
-		downloader.printInfo()
-		return downloader.useSuggestedQueries(10)
-	}).then(() => {
-		downloader.printInfo()
-		return downloader.retrieveImageUrls({auth})
-	}).then(() => {
-		downloader.printInfo()
-		return downloader.saveImages()
-	}).then(() => {
-		downloader.printInfo()
-	}).catch((e) => {
-		console.log('error: ', e)
-	})
-})
+			const {asin, queries, booksPath, auths} = JSON.parse(str);
+	
+			const auth = _.sample(auths);
+	
+			console.log(`== booksPath: ${booksPath} ==`);
+			console.log(`== asin: ${asin} ==`);
+			console.log(`== auth: ${JSON.stringify(auth)} ==`);
+	
+			const downloader = new BookDownloader({booksPath, asin, timeout: 20000});
+	
+			const value = await downloader.init();
+			downloader.printInfo();
+	
+			await downloader.addSuggestedQueries(queries);
+			await downloader.useSuggestedQueries(10);
+			downloader.printInfo();
+	
+			await downloader.retrieveImageUrls({
+				auth,
+				maxQueries: 5
+			});
+			downloader.printInfo();
+	
+			await downloader.saveImages();
+			downloader.printInfo();
+		}
+	} catch (e) {
+		console.log('error: ', e);
+	}
+})();
